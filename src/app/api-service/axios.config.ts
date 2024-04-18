8006a6e8428c6e3a076971f6e8ba2901c7dcaebb
Gia Hai <haidao549@gmail.com>
2024-04-18 18:01:26 +0700

    still fix cors

--- src/app/api-service/axios.config.ts
+++ src/app/api-service/axios.config.ts
@@ -2,16 +2,12 @@ import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === 'production'
-    ? 'https://nextjs-blog-omega-ten-66.vercel.app' ||
-      'https://daogiahai.id.vn'
+    ? 'https://nextjs-blog-omega-ten-66.vercel.app' || 'https://daogiahai.id.vn'
     : 'http://localhost:3000';
 
 const api = axios.create({
   withCredentials: true,
   baseURL: url,
-  headers: {
-    'Access-Control-Allow-Credentials': true,
-  },
 });
 
 export default api;
