099ce2535bcc3e02f4ea777e3958690a9fb7d7e3
Gia Hai <haidao549@gmail.com>
2024-04-18 16:29:48 +0700

    update cors

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://nextjs-blog-boboiboy181.vercel.app',
-          }, // replace this your actual origin
+            value: '*',
+          },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT',
