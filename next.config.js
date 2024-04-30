00fc121e7a44fb3dcdbbb144819409136715edee
Gia Hai <haidao549@gmail.com>
2024-04-30 17:18:17 +0700

    fix domain

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: ['https://ec335.uit.io.vn', 'https://daogiahai.id.vn'],
+            value: '*',
           },
           {
             key: 'Access-Control-Allow-Methods',
